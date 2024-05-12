3bc501aedea0b420be3ee04b2cc30c515aa20657
Tristan <tristapayong@gmail.com>
2024-05-13 02:04:44 +0800

    Set-up

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header >
+        <a href="/">AMAZONA</a>
+      </header>
+      <main>
+        LIST PRODUCTS
+      </main>
+    </div>
+  );
+}
+
+export default App;
